e4214251c75a0238eae777523d2d637034a2702a
Kamil Janik <janikkamil1998@gmail.com>
2026-06-18 16:19:20 +0200

    feat(auth): add nickname uniqueness check

--- src/hooks/use-register.ts
+++ src/hooks/use-register.ts
@@ -1,23 +1,40 @@
 import { createClient } from "@/lib/supabase/client";
-import { EmailPasswordCredentials } from "@/types";
+import { RegisterFormValues } from "@/lib/validations/auth";
 import { useState } from "react";
 
 const useRegister = () => {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const supabase = createClient();
 
-  const register = async ({ email, password }: EmailPasswordCredentials) => {
+  const register = async ({
+    email,
+    password,
+    nickname,
+  }: RegisterFormValues) => {
     setIsLoading(true);
+    nickname = nickname.toLowerCase();
+
+    const { data: exisitng } = await supabase
+      .from("profiles")
+      .select("id")
+      .eq("nickname", nickname)
+      .maybeSingle();
+
+    if (exisitng) {
+      setIsLoading(false);
+      return { error: new Error("Nickname already taken") };
+    }
 
     const { error } = await supabase.auth.signUp({
       email,
       password,
+      options: { data: { nickname } },
     });
 
     setIsLoading(false);
-
     return { error };
   };
+
   return { register, isLoading };
 };
 
