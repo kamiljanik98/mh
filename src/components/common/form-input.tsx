5833d9548b59590a3eb46d389857ac6ce7df91df
Kamil Janik <janikkamil1998@gmail.com>
2026-06-25 10:25:36 +0200

    fix(upload): update FormInput imports after rename

---
+++ src/components/common/form-input.tsx
@@ -0,0 +1,41 @@
+import { Control, Controller, FieldValues, Path } from "react-hook-form";
+import { Input } from "../ui/input";
+import { Field, FieldLabel, FieldError } from "@/components/ui/field";
+
+type FormInputProps<T extends FieldValues> = {
+  name: Path<T>;
+  control: Control<T>;
+  label: string;
+} & React.ComponentProps<typeof Input>;
+
+const FormInput = <T extends FieldValues>({
+  name,
+  control,
+  label,
+  ...props
+}: FormInputProps<T>) => {
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState }) => (
+        <Field data-invalid={fieldState.invalid}>
+          <FieldLabel className="text-xs" htmlFor={field.name}>
+            {label}
+          </FieldLabel>
+          <Input
+            {...field}
+            {...props}
+            value={field.value ?? ""}
+            id={field.name}
+            aria-invalid={fieldState.invalid}
+            className="pb-2.5 pt-0.5"
+          />
+          {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+        </Field>
+      )}
+    />
+  );
+};
+
+export default FormInput;
