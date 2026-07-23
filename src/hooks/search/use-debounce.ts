463a3ff08d762f3c080e4a025c76f069c3f2f89c
jukka <janikkamil1998@gmail.com>
2026-07-23 13:01:14 +0200

    feat(search): add debounce hook

---
+++ src/hooks/search/use-debounce.ts
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+
+export const useDebounce = <T>(value: T, delay = 300): T => {
+  const [debounceValue, setDebounceValue] = useState(value);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebounceValue(value), delay);
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+
+  return debounceValue;
+};
