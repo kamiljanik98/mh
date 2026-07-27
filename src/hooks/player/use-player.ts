0dfea2ead31218f4fb45434718742ae069a48e21
jukka <janikkamil1998@gmail.com>
2026-07-27 16:16:36 +0200

    feat(player): add isPlaying, progress, duration and seek state to player store

--- src/hooks/player/use-player.ts
+++ src/hooks/player/use-player.ts
@@ -6,10 +6,19 @@ interface PlayerStore {
   activeId: string | null;
   songs: Song[];
   volume: number;
+  isPlaying: boolean;
+  progress: number;
+  duration: number;
+  seekTo: number | null;
   setActiveId: (id: string) => void;
   setIds: (ids: string[]) => void;
   setSongs: (songs: Song[]) => void;
   setVolume: (volume: number) => void;
+  setIsPlaying: (isPlaying: boolean) => void;
+  setProgress: (progress: number) => void;
+  setDuration: (duration: number) => void;
+  requestSeek: (progress: number) => void;
+  clearSeekRequest: () => void;
   reset: () => void;
 }
 
@@ -18,11 +27,20 @@ const usePlayer = create<PlayerStore>((set) => ({
   activeId: null,
   songs: [],
   volume: 1,
+  isPlaying: false,
+  progress: 0,
+  duration: 0,
+  seekTo: null,
   setActiveId: (id) => set({ activeId: id }),
   setIds: (ids) => set({ ids }),
   setSongs: (songs) => set({ songs }),
   setVolume: (volume) => set({ volume }),
-  reset: () => set({ ids: [], activeId: null, songs: [], volume: 1 }),
+  setIsPlaying: (isPlaying) => set({ isPlaying }),
+  setProgress: (progress) => set({ progress }),
+  setDuration: (duration) => set({ duration }),
+  requestSeek: (progress) => set({ seekTo: progress }),
+  clearSeekRequest: () => set({ seekTo: null }),
+  reset: () => set({ ids: [], activeId: null, songs: [] }),
 }));
 
 export default usePlayer;
