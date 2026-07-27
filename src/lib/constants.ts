94daa82f8fd82d37182066d9beea0a90a3882fae
jukka <janikkamil1998@gmail.com>
2026-07-27 15:51:01 +0200

    feat(constants): add image placeholder paths

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -13,3 +13,8 @@ export const ACCEPTED_IMAGE = {
 export const DASHED_BORDER = {
   backgroundImage: `url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' stroke='%23ffffff4d' stroke-width='2' stroke-dasharray='6%2c 10' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e")`,
 } as const;
+
+export const IMAGE_PLACEHOLDER = {
+  COVER: "/cover-placeholder.jpg",
+  AVATAR: "/avatar-placeholder.jpg",
+} as const;
