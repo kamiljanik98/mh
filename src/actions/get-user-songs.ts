3479d7145f3945e3815d97db17812c85c7cb9b4f
jukka <janikkamil1998@gmail.com>
2026-07-27 16:36:10 +0200

    feat(profile): add get-user-songs server action

---
+++ src/actions/get-user-songs.ts
@@ -0,0 +1,17 @@
+import { createClient } from "@/lib/supabase/server";
+import { Song } from "@/types";
+import { profile } from "console";
+
+export async function getUserSongs(userId: string): Promise<Song[]> {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase
+    .from("songs")
+    .select("*")
+    .eq("uploaded_by", userId)
+    .order("created_at", { ascending: false });
+
+  if (error || !data) return [];
+
+  return data.map((song) => ({ ...song, profiles: null }));
+}
